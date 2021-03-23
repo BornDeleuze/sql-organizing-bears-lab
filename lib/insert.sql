98c293c8f540d856cec846c4b1c8e29045535127
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-23 17:29:42 -0400

    commit tree 4d5a91c747d07a794ec579fc97510c5a73a351f7

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (1, "Mr. Chocolate", 15, "M", "White", "Happy", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (2, "Rowdy", 2, "M", "Black", "Sad", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (3, "Tabitha", 9, "F", "Zebra", "Grumpy", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (4, "Sergeant Brown", 700, "F", "Teal", "Hungry", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (5, "Melissa", 666, "F", "Purple", "Bashful", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (6, "Grinch", 8, "M", "Blue", "Dopey", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (7, "Wendy", 0, "F", "Brown", "Shy", 0);
+INSERT INTO bears (id, name, age, sex, color, temperament, alive) VALUES (8, NULL, 1000, "M", "Red", "Enraged", 0);
